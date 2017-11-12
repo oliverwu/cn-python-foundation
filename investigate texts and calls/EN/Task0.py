1d85f90876f409e4c31ede07060115a1a93e6dd1
oliver <oliver.wu.1987@gmail.com>
2017-11-12 23:30:28 +0800

    Finish the Task0 Print the message of the first record of texts and the last record of calls

--- investigate texts and calls/EN/Task0.py
+++ investigate texts and calls/EN/Task0.py
@@ -2,13 +2,13 @@
 Intro to Python Project 1, Task 0
 
 Complete each task in the file for that task. Submit the whole folder
-as a zip file or GitHub repo. 
+as a zip file or GitHub repo.
 Full submission instructions are available on the Project Preparation page.
 """
 
 
 """
-Read file into texts and calls. 
+Read file into texts and calls.
 It's ok if you don't understand how to read files
 You will learn more about reading files in future lesson
 """
@@ -23,10 +23,12 @@ with open('calls.csv', 'r') as f:
 
 
 """
-TASK 0: 
+TASK 0:
 what is the first record of texts and what is the last record of calls
-Print messages: 
+Print messages:
 "First record of texts, <incoming number> texts <answering number> at time <time>"
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
 
+print("First record of texts, {} texts {} at time {}".format(texts[0][0],texts[0][1],texts[0][2]))
+print("Last record of calls, {} calls {} at time {}, lasting {} seconds".format(calls[-1][0],calls[-1][1],calls[-1][2],calls[-1][3]))
