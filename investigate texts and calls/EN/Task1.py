b2bb0ea1d61e0d6a3b846a99b9837975446ab2bb
oliver <oliver.wu.1987@gmail.com>
2017-11-12 23:47:14 +0800

    Finish the second task Find out how many different telphone numbers in the records

--- investigate texts and calls/EN/Task1.py
+++ investigate texts and calls/EN/Task1.py
@@ -2,13 +2,13 @@
 Intro to Python Project 1, Task 1
 
 Complete each task in the file for that task. Submit the whole folder
-as a zip file or GitHub repo. 
+as a zip file or GitHub repo.
 Full submission instructions are available on the Project Preparation page.
 """
 
 
 """
-Read file into texts and calls. 
+Read file into texts and calls.
 It's ok if you don't understand how to read files
 You will learn more about reading files in future lesson
 """
@@ -23,8 +23,23 @@ with open('calls.csv', 'r') as f:
 
 
 """
-TASK 1: 
-How many different telephone numbers are there in the records? 
-Print a message: 
+TASK 1:
+How many different telephone numbers are there in the records?
+Print a message:
 "There are <count> different telephone numbers in the records."
 """
+def amount_telnum(texts,calls):
+    telnum_records = []
+    for text in texts:
+        if text[0] not in telnum_records:
+            telnum_records.append(text[0])
+        if text[1] not in telnum_records:
+            telnum_records.append(text[1])
+    for call in calls:
+        if call[0] not in telnum_records:
+            telnum_records.append(call[0])
+        if call[1] not in telnum_records:
+            telnum_records.append(call[1])
+    return len(telnum_records)
+
+print("There are {} different telephone numbers in the records.".format(amount_telnum(texts,calls)))
