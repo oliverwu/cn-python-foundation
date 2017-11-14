a178d722e71159ca49b7700148895039a0fc7442
oliver <oliver.wu.1987@gmail.com>
2017-11-14 17:08:43 +0800

    Finish the fifth task(Task4). Find out the list if telemarketers numbers.

--- investigate texts and calls/EN/Task4.py
+++ investigate texts and calls/EN/Task4.py
@@ -2,12 +2,12 @@
 Intro to Python Lab 1, Task 4
 
 Complete each task in the file for that task. Submit the whole folder
-as a zip file or GitHub repo. 
+as a zip file or GitHub repo.
 Full submission instructions are available on the Lab Preparation page.
 """
 
 """
-Read file into texts and calls. 
+Read file into texts and calls.
 It's ok if you don't understand how to read files
 You will learn more about reading files in future lesson
 """
@@ -24,13 +24,35 @@ with open('calls.csv', 'r') as f:
 """
 TASK 4:
 The telephone company want to identify numbers that might be doing
-telephone marketing. Create a set of possible telemarketers: 
+telephone marketing. Create a set of possible telemarketers:
 these are numbers that make outgoing calls but never send texts,
 receive texts or receive incoming calls.
 
-Print a message: 
+Print a message:
 "These numbers could be telemarketers: "
 <list of numbers>
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
 
+
+def outgoing_call_telenum_pool(calls):
+    outgoing_call_telenum_pool = []
+    for call in calls:
+        if call[0] not in outgoing_call_telenum_pool:
+            outgoing_call_telenum_pool.append(call[0])
+    return set(outgoing_call_telenum_pool)
+
+def find_possible_telemarketers(calls,texts):
+    telenum_pool = outgoing_call_telenum_pool(calls)
+    for call in calls:
+        if call[1] in telenum_pool:
+            telenum_pool.remove(call[1])
+    for text in texts:
+        if text[0] in texts:
+            telenum_pool.remove(text[0])
+        elif text[1] in texts:
+            telenum_pool.remove(text[1])
+    return '\n'.join(sorted(telenum_pool))
+
+possible_telemarkets_list = find_possible_telemarketers(calls,texts)
+print("These numbers could be telemarketers:\n{}".format(possible_telemarkets_list))
